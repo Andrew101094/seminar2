ea9cfadebdf98f0f6674f43a141fcb46400a4212
Andrew <goscontract01@yandex.ru>
2023-07-12 18:21:09 +0300

    Zadacha10

--- Program.cs
+++ Program.cs
@@ -1,52 +1,27 @@
-﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает
-// наибольшую цифру числа.
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру числа.
 
 // Например:
-// 78 -> 8
-// 12-> 2
-// 85 -> 8
+// 456 -> 5
 
 int GetRandomNumber()
 {
-int number = new Random().Next(10, 100);
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
 return number;
 }
 
 int GetFirstDigit(int number1)
 {
-return number1 / 10;
+    return number1/10;
 }
 
-int GetLastDigit(int number1)
+int GetSecondDigit(int number2)
 {
-return number1 % 10;
-}
-
-void PrintNumber(int num1)
-{
-Console.Write(num1 + " ");
-}
-void ShowLargestDigit(int digit1, int digit2)
-{
-if (digit1 > digit2)
-{
-Console.Write("-> " + digit1);
-}
-else Console.Write("-> " + digit2);
+    return number2%10;
 }
 
 int num = GetRandomNumber();
-PrintNumber(num);
 int firstDigit = GetFirstDigit(num);
-//PrintNumber(firstDigit);
-int lastDigit = GetLastDigit(num);
-//PrintNumber(lastDigit);
-ShowLargestDigit(firstDigit, lastDigit);
+int secondDigit = GetSecondDigit(firstDigit);
+Console.WriteLine(secondDigit);
 
-Console.WriteLine();
-
-int num2 = GetRandomNumber();
-PrintNumber(num2);
-int firstDigit2 = GetFirstDigit(num2);
-int lastDigit2 = GetLastDigit(num2);
-ShowLargestDigit(firstDigit2, lastDigit2);
